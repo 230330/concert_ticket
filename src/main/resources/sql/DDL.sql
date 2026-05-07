195e8efdf29713ab6051274ae54519e40ad60029
hzf <3123958330@qq.com>
2026-05-07 16:43:31 +0800

    @hzf@调整场次表show的状态枚举

--- src/main/resources/sql/DDL.sql
+++ src/main/resources/sql/DDL.sql
@@ -1,2 +1,4 @@
 -- 为 user 表添加昵称最后修改时间字段
 ALTER TABLE `user` ADD COLUMN `nickname_last_modified` datetime DEFAULT NULL COMMENT '昵称最后修改时间' AFTER `avatar`;
+-- 2026-5-7 添加场次状态字段
+ALTER TABLE concert_ticket.`show` MODIFY COLUMN status tinyint DEFAULT 1 NOT NULL COMMENT '状态：0未开售 1售票中 2已售罄 3已结束 4已取消';
